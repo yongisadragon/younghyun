baa264e4ec973d052ef6895b46752533b456841e
yongisagragon <ckfflekfxms@gmail.com>
2024-01-16 18:29:18 +0900

    worksindex 임시 스타일

--- src/components/WorksIndex.jsx
+++ src/components/WorksIndex.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import worksData from "../data/worksData";
+import styles from "./WorksIndex.module.css";
 export default function WorksIndex() {
   return (
-    <div>
+    <div className={styles.container}>
       {worksData?.map((work) => (
         <NavLink key={work.id} to={`/works/work${work.id}`}>
           {work.title}
