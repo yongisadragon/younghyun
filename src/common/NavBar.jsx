db02bee8094fe3983eb76081cff9215ae26bda5d
yongisagragon <ckfflekfxms@gmail.com>
2024-01-16 18:23:17 +0900

    Navbar 태그 통일 및 기능 설정

--- src/common/NavBar.jsx
+++ src/common/NavBar.jsx
@@ -3,15 +3,25 @@ import { NavLink, useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./NavBar.module.css";
 export default function NavBar() {
   const [searchAbout, setSearchAbout] = useSearchParams();
-  const handleAboutClick = () => {
-    setSearchAbout({ about: "leeyounghyun" });
-  };
+  const navigate = useNavigate();
 
   return (
-    <div className={styles.layout}>
-      <NavLink to={"/"}>Works</NavLink>
-      <div onClick={handleAboutClick}>About</div>
-      <div>Instagram</div>
+    <div className={styles.container}>
+      <div className={styles.links}>
+        <div onClick={() => navigate("/")}>Works</div>
+        <div
+          onClick={() => {
+            setSearchAbout({ about: "leeyounghyun" });
+          }}>
+          About
+        </div>
+        <div
+          onClick={() => {
+            window.open("https://www.instagram.com/leeygnhn/", "_blank");
+          }}>
+          Instagram
+        </div>
+      </div>
     </div>
   );
 }
