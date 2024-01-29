b166b686996cb627280d4963f956e843e4639961
yongisagragon <ckfflekfxms@gmail.com>
2024-01-29 21:55:22 +0900

    lazy 임시 중지

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { Suspense, useState, lazy, useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import NavBar from "./common/navBar/NavBar";
-// import Works from "./components/works/Works";
+import Works from "./components/works/Works";
 import About from "./pages/about/About";
 
 export default function App() {
   const [isAbout, setIsAbout] = useState(false);
-  const Works = lazy(() => import("./components/works/Works"));
+  // const Works = lazy(() => import("./components/works/Works"));
 
   if (isAbout) {
     document.body.style.overflow = "hidden";
@@ -17,17 +17,15 @@ export default function App() {
 
   return (
     <>
-      <Suspense fallback={<>..Loading</>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/works" element={<Home />} />
-          <Route path="/works/:workTitle" element={<Works />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-        <br />
-        <NavBar setIsAbout={setIsAbout} />
-        {isAbout && <About setIsAbout={setIsAbout} />}
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/works" element={<Home />} />
+        <Route path="/works/:workTitle" element={<Works />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      <br />
+      <NavBar setIsAbout={setIsAbout} />
+      {isAbout && <About setIsAbout={setIsAbout} />}
     </>
   );
 }
