6ea1ac2454be2c44b633b37a863cd8789fabcf00
yongisagragon <ckfflekfxms@gmail.com>
2024-02-06 21:30:29 +0900

    스크롤 감지 기능 진행중

--- src/components/worksIndex/WorksIndex.jsx
+++ src/components/worksIndex/WorksIndex.jsx
@@ -1,10 +1,25 @@
-import React, { forwardRef, useState } from "react";
+import React, { forwardRef, useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import worksData from "../../data/worksData";
 import styles from "./WorksIndex.module.css";
 const WorksIndex = (props, ref) => {
   const [currentScroll, setCurrentScroll] = useState(false);
   const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   const scrollTo = () => {
+  //     const scrollPosition = window.scrollY;
+  //     // 썸네일들의 Y포지션값 계산해서 배열
+  //     const thumbnailsY = ref.current.map((thumb) => thumb.offsetTop);
+
+  //     //현재 스크롤위치의 idx계산
+  //     const currentIndex = thumbnailsY.findIndex((el) => el > scrollPosition);
+
+  //     console.log(currentIndex);
+  //   };
+  //   scrollTo();
+  // }, [ref]);
+
   const handleScroll = (idx) => {
     ref.current[idx].scrollIntoView({ behavior: "smooth" });
     setCurrentScroll(ref.current[idx]);
@@ -20,15 +35,15 @@ const WorksIndex = (props, ref) => {
     <div className={styles.container}>
       {worksData.map((work, idx) => (
         <div
+          key={work.id}
+          className={
+            currentScroll === ref.current[idx] ? styles.selected : styles.links
+          }
           onClick={() => {
             currentScroll === ref.current[idx]
               ? handleLinkTo(work.name, idx)
               : handleScroll(idx);
-          }}
-          className={
-            currentScroll === ref.current[idx] ? styles.selected : styles.links
-          }
-          key={work.id}>
+          }}>
           {`${work.id}. ${work.name.replace("-", " ")}`}
         </div>
       ))}
