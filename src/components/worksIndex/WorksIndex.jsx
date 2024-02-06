c73f8c41a077f35de13e8324f6b6aa1170332683
Lee Sang Yong <ckfflekfxms@gmail.com>
2024-02-06 23:55:24 +0900

    스크롤 액션 진행중. 첫번째 마지막 인덱스 무반응

--- src/components/worksIndex/WorksIndex.jsx
+++ src/components/worksIndex/WorksIndex.jsx
@@ -3,46 +3,46 @@ import { NavLink, useNavigate } from "react-router-dom";
 import worksData from "../../data/worksData";
 import styles from "./WorksIndex.module.css";
 const WorksIndex = (props, ref) => {
-  const [currentScroll, setCurrentScroll] = useState(false);
+  const [currentPosition, setCurrentPosition] = useState(null);
   const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   const scrollTo = () => {
-  //     const scrollPosition = window.scrollY;
-  //     // 썸네일들의 Y포지션값 계산해서 배열
-  //     const thumbnailsY = ref.current.map((thumb) => thumb.offsetTop);
+  useEffect(() => {
+    const handlePosition = () => {
+      const scrollPosition = window.scrollY;
+      // 썸네일들의 Y포지션값 계산해서 배열
+      const thumbnailsY = ref.current.map((thumb) => thumb.offsetTop);
+      //현재 스크롤위치의 idx계산
+      const currentIndex = thumbnailsY.findIndex((el) => el > scrollPosition);
 
-  //     //현재 스크롤위치의 idx계산
-  //     const currentIndex = thumbnailsY.findIndex((el) => el > scrollPosition);
+      setCurrentPosition(currentIndex === -1 ? null : currentIndex);
+    };
 
-  //     console.log(currentIndex);
-  //   };
-  //   scrollTo();
-  // }, [ref]);
+    window.addEventListener("scroll", handlePosition);
+    return () => window.removeEventListener("scroll", handlePosition);
+  }, [ref]);
 
-  const handleScroll = (idx) => {
-    ref.current[idx].scrollIntoView({ behavior: "smooth" });
-    setCurrentScroll(ref.current[idx]);
+  const handleScroll = (i) => {
+    ref.current[i].scrollIntoView({ behavior: "smooth" });
   };
 
-  const handleLinkTo = (workName, i) => {
-    if (currentScroll === ref.current[i]) {
+  const handleLinkTo = (workName, idx) => {
+    if (currentPosition === ref.current[idx]) {
       navigate(`/works/${workName}`);
+    } else {
+      handleScroll(idx);
     }
   };
+
+  console.log(currentPosition);
 
   return (
     <div className={styles.container}>
       {worksData.map((work, idx) => (
         <div
           key={work.id}
-          className={
-            currentScroll === ref.current[idx] ? styles.selected : styles.links
-          }
+          className={currentPosition === idx ? styles.selected : styles.links}
           onClick={() => {
-            currentScroll === ref.current[idx]
-              ? handleLinkTo(work.name, idx)
-              : handleScroll(idx);
+            handleLinkTo(work.name, idx);
           }}>
           {`${work.id}. ${work.name.replace("-", " ")}`}
         </div>
